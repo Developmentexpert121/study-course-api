5499273fb232ce90d7b03153e7cda3e84310de46
Developmentexpert121 <developmentexpert121@gmail.com>
2025-09-24 11:33:13 +0530

    push code

--- src/util/dbConn.ts
+++ src/util/dbConn.ts
@@ -1,41 +1,29 @@
-"use strict";
-import { Sequelize } from "sequelize";
-// import association from '../models/associations';
-import fs from "fs";
-import path from "path";
+import { Sequelize } from 'sequelize';
 
-const process = require("process");
-const env = process.env.NODE_ENV || "local";
-const config = require("../conf/config");
-// import config from '../conf/config'
-const db: any = {};
+const env = process.env.NODE_ENV || 'development';
+const config = require('../conf/config.js')[env];
 
-let sequelize: any;
+let sequelize;
+
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // Production with DATABASE_URL
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'postgres', // ← THIS MUST BE PRESENT
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  });
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  //    {
-  //   ...config,
-  //   dialectOptions: {
-  //     ssl: {
-  //       require: true,
-  //       rejectUnauthorized: false,
-  //     },
-  //   },
-  // }
-  );
+  // Development
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'postgres', // ← AND THIS MUST BE PRESENT
+    logging: env === 'development'
+  });
 }
-
-// Get the list of all model files
-console.log(__dirname);
-const modelsPath = path.join(__dirname, "../models");
-const modelFiles = fs
-  .readdirSync(modelsPath)
-  .filter((file) => file.endsWith(".model.ts"));
 
 export default sequelize;
