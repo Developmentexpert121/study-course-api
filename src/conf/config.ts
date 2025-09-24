57909adc673e6514ffd7344a0e77c8c4997aba31
Developmentexpert121 <developmentexpert121@gmail.com>
2025-09-24 11:25:36 +0530

    push code

--- src/conf/config.ts
+++ src/conf/config.ts
@@ -3,10 +3,10 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'course-app',
-    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres"
   },
   production: {
