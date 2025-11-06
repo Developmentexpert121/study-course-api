91f10efa217dc131f61ae62238b7bdd8dffe32b6
devexabhinav <abhinavkumar@devexhub.in>
2025-11-06 09:11:40 +0530

    build issue

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,51 +1,17 @@
-// // types/express/index.d.ts
-// import type User from '../../src/models/user.model';
-// import { File } from 'multer';
-
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       user?: {
-//         id: number;
-//         email?: string;
-//         role?: string;
-//       };
-//       file?: File;
-//       files?: File[] | { [fieldname: string]: File[] };
-//     }
-//   }
-// }
-
-
-
 // types/express/index.d.ts
-
-// types/express/index.d.ts
-import { File } from 'multer';
 import type User from '../../src/models/user.model';
+import { File } from 'multer';
 
 export declare global {
   namespace Express {
     interface Request {
       user?: {
-        id: string;
+        id: number;
         email?: string;
         role?: string;
       };
-    }
-    
-    namespace Multer {
-      interface File {
-        fieldname: string;
-        originalname: string;
-        encoding: string;
-        mimetype: string;
-        size: number;
-        destination: string;
-        filename: string;
-        path: string;
-        buffer: Buffer;
-      }
+      file?: File;
+      files?: File[] | { [fieldname: string]: File[] };
     }
   }
 }
