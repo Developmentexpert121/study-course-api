3c0d44c5bdef9cdec4e79adb5a7c62d67cb03f21
devexabhinav <abhinavkumar@devexhub.in>
2025-11-06 11:39:30 +0530

    fix build issue

---
+++ src/migrations/20251106060752-create-rating-model.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Ratings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      score: Sequelize.INTEGER,
+      userId: Sequelize.INTEGER,
+      courseId: Sequelize.INTEGER,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Ratings');
+  }
+};
