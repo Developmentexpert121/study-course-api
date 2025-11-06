190c2754e754ccc3c5402a94c730ff4e1c176bbe
devexabhinav <abhinavkumar@devexhub.in>
2025-11-06 11:33:21 +0530

    fix build issue

--- src/migrations/20251030075649-update-rating.js
+++ src/migrations/20251030075649-update-rating.js
@@ -1,16 +1,15 @@
+'use strict';
 
-// 'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('ratings', 'isactive', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    });
+  },
 
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await queryInterface.addColumn('Ratings', 'isactive', {
-//       type: Sequelize.BOOLEAN,
-//       defaultValue: true,
-//       allowNull: false
-//     });
-//   },
-
-//   async down(queryInterface, Sequelize) {
-//     await queryInterface.removeColumn('Ratings', 'isactive');
-//   }
-// };
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('ratings', 'isactive');
+  }
+};
