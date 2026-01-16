bd0ce987f8414edf4090a2f2ca3e448bac4a4841
devexabhinav <abhinavkumar@devexhub.in>
2026-01-16 17:52:00 +0530

    stash

--- src/helpers/certificate.helper.ts
+++ src/helpers/certificate.helper.ts
@@ -112,8 +112,9 @@ export async function generateCertificatePDFAndUpload({
 
         console.log('🌐 Launching browser for PDF generation...');
         browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome-stable',
+            headless: true,
             args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            headless: true
         });
 
         const page = await browser.newPage();
